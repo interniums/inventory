1569f5db8e82de19f80c70ff938ceee3c8d4e2af
interniums <intuition228@gmail.com>
2024-07-02 09:05:01 +0300

    1

---
+++ models/item.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxLength: 24,
+  },
+  description: {
+    type: String,
+    maxLength: 999,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  price: {
+    type: String,
+  },
+  numberInStock: {
+    type: Number,
+  },
+})
+
+ItemSchema.virtual("url").get(function () {
+  return `/category/item${this._id}`
+})
+
+module.exports = mongoose.model("Item", ItemSchema)
